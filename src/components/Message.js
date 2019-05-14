e12adfde6180de78fb847abd6a082b5c5fd4d02a
ngo_huu_phong <ngo_huu_phong@management-partners.co.jp>
2019-05-14 16:58:48 +0700

    Phan Chia Components

---
+++ src/components/Message.js
@@ -0,0 +1,13 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { Component } from 'react';
+class Message extends Component{
+	render(){
+		return (
+			<h3>
+				<span className="badge amber darken-2">Mua Hàng Thành Công !</span>
+			</h3>
+		);
+	}
+}
+
+export default Message;
